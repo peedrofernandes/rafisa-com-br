2c572ca413b7c0048be782332cc5ece9c8eb1d06
peedrofernandes <ppedroffaria2002@gmail.com>
2021-05-07 09:34:25 -0300

    Botao whatsapp (finalização)

--- rafisa-app/components/WhatsAppButton.js
+++ rafisa-app/components/WhatsAppButton.js
@@ -2,10 +2,20 @@
 
 export default function WhatsAppButton(props) {
   return (
-    <div className="wpp">
-      <h4>Faça um orçamento!</h4>
-      <p>Horário de atendimento: das 08:00 às 18:00</p>
-      <img src="/assets/icons/whatsapp-color.svg"></img>
-    </div>
+      <div className="wpp-container">
+        <div>
+          <label htmlFor="close">
+            <img className="cancel" src="/assets/icons/cancel.svg"></img>
+          </label>
+        </div>
+
+        <div className="wpp">
+          <h4>Faça um orçamento!</h4>
+          <img className="whatsimg" src="/assets/icons/whatsapp-color.svg"></img>
+          <p>Horário de atendimento: das 08:00 às 18:00</p>
+        </div>
+
+      </div>
+
   )
 }
