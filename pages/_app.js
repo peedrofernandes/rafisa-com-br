011d238fad57d9bdeb2335f5510f9cb25ade7c60
peedrofernandes <ppedroffaria2002@gmail.com>
2021-06-15 14:28:21 -0300

    Rafisa 1.0.7

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/styles.css'
 import DataProvider from '../store/GlobalState'
 import ReactGA from 'react-ga';
 import { useEffect } from 'react';
+import Head from 'next/head'
 
 const trackingID = process.env.TRACKING_GA_ID;
 
@@ -15,6 +16,11 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <DataProvider>
+      <Head>
+        <title>Rafisa - Comércio de Sacarias LTDA - Atendimento de segunda à sexta das 08:00 às 18:00!</title>
+        <meta name="viewport" content="initial-scale-1.0, width=device-width"></meta>
+        <meta name="description" content="Na Rafisa, seu produto é a nossa preocupação - Temos sacarias convencionais, com revestimento extra e até estampamos sua marca nas suas embalagens, porque quem não é visto não é lembrado! Sacarias impressas, laminadas e convencionais você só encontra na Rafisa."></meta>
+      </Head>
       <Component {...pageProps} />
     </DataProvider>
   )
