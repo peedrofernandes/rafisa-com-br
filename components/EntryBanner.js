860eadd2451d58c8c86005ff1b855d6918bfcd02
pedro <ppedroffaria2002@gmail.com>
2022-04-24 17:21:34 -0300

    Testing CI/CD pipeline

--- components/EntryBanner.js
+++ components/EntryBanner.js
@@ -2,7 +2,7 @@ export default function EntryBanner(props) {
   return (
     <div className="entry">
       <h3>Venha para a Rafisa!</h3>
-      <h1>Comodidade esculpida em embalagem</h1>
+      {/* <h1>Comodidade esculpida em embalagem</h1> */}
       <div className="buttons">
         <ul>
           <li>Ver produtos</li>
