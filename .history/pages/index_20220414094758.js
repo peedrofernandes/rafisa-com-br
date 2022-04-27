49db96bff28f2a73c33f95b60964c17f371efd89
Pedro <ppedroffaria2002@gmail.com>
2022-04-26 23:11:34 -0300

    Whatsapp number correction

--- .history/pages/index_20220414094758.js
+++ .history/pages/index_20220414094758.js
@@ -59,7 +59,7 @@ export default function Home(props) {
           allowFullScreen=""
           loading="lazy"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2949.214627927318!2d-48.842221427079714!3d-26.34560660194376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94deb1a7aa112f43%3A0xe2eec0258506539!2sRafisa%20Com%C3%A9rcio%20de%20Sacarias!5e0!3m2!1spt-BR!2sbr!4v1623527275272!5m2!1spt-BR!2sbr">
-        </iframe>
+          </iframe>
 
       </main>
 
