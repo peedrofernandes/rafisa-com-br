40406972d24592f5389dc73eaef42aaa2a2f2374
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2023-04-15 13:14:01 -0300

    Shortened <head> title and description

--- pages/_app.js
+++ pages/_app.js
@@ -17,8 +17,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <title>Rafisa - Embalagens de Ráfia - Armazenamento de Fertilizantes, rações, farináceos, entulhos, sementes, cereais, entulho e muitos outros!</title>
-        <meta name="description" content="Sacos de ráfia para armazenamento de Fertilizantes, rações, farinha, entulhos, sementes, cereais, conexões, sal, peças metalúrgicas, materiais de construção civil, contenção de barrancos, dentre muitos outros. Clique aqui e conheça a Rafisa!"/>
+        <title>Rafisa - Embalagens de Ráfia</title>
+        <meta name="description" content="Sacos de ráfia para armazenamento de Fertilizantes, rações, entulhos, cereais, sal, peças metálicas, materiais de construção civil, contenção de barrancos, dentre muitos outros!"/>
         <link rel="canonical" href="https://rafisa.com.br/"/>
       </Head>
     <DataProvider>
