4a05c473bd01aa2de66d36e4a85b7a71fd2df8ae
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2023-04-15 12:59:17 -0300

    Added <head> tag on app - Title, meta description and canonicalization

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/styles.css'
 import DataProvider from '../store/GlobalState'
 import ReactGA from 'react-ga';
 import { useEffect } from 'react';
+import Head from "next/head"
 
 const trackingID = process.env.TRACKING_GA_ID;
 
@@ -14,9 +15,16 @@ function MyApp({ Component, pageProps }) {
   }, [])
 
   return (
+    <div>
+      <Head>
+        <title>Rafisa - Embalagens de Ráfia - Armazenamento de Fertilizantes, rações, farináceos, entulhos, sementes, cereais, entulho e muitos outros!</title>
+        <meta name="description" content="Sacos de ráfia para armazenamento de Fertilizantes, rações, farinha, entulhos, sementes, cereais, conexões, sal, peças metalúrgicas, materiais de construção civil, contenção de barrancos, dentre muitos outros. Clique aqui e conheça a Rafisa!"/>
+        <link rel="canonical" href="https://rafisa.com.br/"/>
+      </Head>
     <DataProvider>
       <Component {...pageProps} />
     </DataProvider>
+    </div>
   )
 }
 
